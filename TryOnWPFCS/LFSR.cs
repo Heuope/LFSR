d8a5bb6919e13e2309ffef79e66455c1d07cc414
Kostya <konstantin.sedniov@gmail.com>
2019-10-09 23:21:55 +0300

    Initial commit

---
+++ TryOnWPFCS/LFSR.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryOnWPFCS
+{
+    static class LFSR
+    {
+        // x^29 + x^2 + 1   
+        public static byte[] Key;
+        public static byte[] InitialFile;
+        public static byte[] CipherFile;
+        private const int BitInByte = 8;
+
+        public static void MainPoint(string _key, string _filePathLoad, string _filePathSave)
+        {
+            InitialFile = System.IO.File.ReadAllBytes(_filePathLoad);
+            KeyGenerator(_key);
+            GenerateCipher();
+            System.IO.File.WriteAllBytes(_filePathSave, CipherFile);
+        }
+
+        static private void KeyGenerator(string keyStream)
+        {
+            var _key = new byte[InitialFile.Length];
+
+            //
+            // TODO create block for small files without using generator 
+            //
+
+            var _keyStream = new byte[keyStream.Length];
+
+            for (int i = 0; i < keyStream.Length; i++)
+                _keyStream[i] = byte.Parse(keyStream[i].ToString());
+
+            string _bitsKey = "";
+            int _lastNumber;
+            int k = 0; // index in _key
+
+            for (Int64 i = 0; i < InitialFile.Length * BitInByte; i++)
+            {
+                _bitsKey += _keyStream[0].ToString();
+                if (_bitsKey.Length == 8)
+                {
+                    _key[k++] = Convert.ToByte(_bitsKey, 2);
+                    _bitsKey = "";
+                }                   
+                _lastNumber = _keyStream[0] ^ _keyStream[27];
+
+                for (int j = 0; j < _keyStream.Length - 1; j++)
+                    _keyStream[j] = _keyStream[j + 1];
+                
+                _keyStream[_keyStream.Length - 1] = (byte)_lastNumber;
+            }
+
+            Key = _key;
+        }
+
+        static private void GenerateCipher()
+        {
+            var _cipherFile = new byte[InitialFile.Length];
+                       
+            for (int i = 0; i < InitialFile.Length; i++)
+                _cipherFile[i] = (byte)(InitialFile[i] ^ Key[i]);
+
+            CipherFile = _cipherFile;
+        }
+    }
+}
---
+++ TryOnWPFCS/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TryOnWPFCS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string LoadFilePath;
+        private string SaveFilePath;
+
+        public MainWindow()
+        {
+            InitializeComponent();            
+        }
+
+        private void text_box_LFSR_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var _text = (TextBox)sender;
+            string _str = "";
+
+            foreach (var item in _text.Text)
+                if (item == '1' || item == '0')
+                    _str += item;
+
+            _text.Text = _str;
+            _text.CaretIndex = _text.Text.Length;
+        }
+        private void Load_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.Filter = "Text documents (*.txt)|*.txt|All files (*.*)|*.*";
+            dialog.FilterIndex = 2;
+
+            Nullable<bool> result = dialog.ShowDialog();
+
+            if (result == true)
+                LoadFilePath = dialog.FileName;
+        }
+
+        private void Save_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.SaveFileDialog();            
+
+            Nullable<bool> result = dialog.ShowDialog();
+
+            if (result == true)
+                SaveFilePath = dialog.FileName;
+        }
+
+        private void Button_Click_LFSR(object sender, RoutedEventArgs e)
+        {
+            LFSR.MainPoint(LFSR_Key.Text, LoadFilePath, SaveFilePath);
+            //StartBits.Text = LFSR.Text;
+            //KeyBits.Text = LFSR.Key;
+            //FinishBits.Text = LFSR.CipherText;
+        }
+    }
+}
